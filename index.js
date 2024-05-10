fe6bb2642310186744cfb0e0e241fe0f4d84e90e
ftgyhuji8bh <gs1wilma953@hotmail.com>
2024-05-10 11:17:08 +0700

    commit

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-const countOccurrences = (arr, value) =>
-  arr.reduce((a, v) => (v === value ? a + 1 : a), 0);
+function countConsonants(str) {
+  return str.match(/[bcdfghjklmnpqrstvwxyz]/gi).length;
+}
