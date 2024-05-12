019e2d02aadb8c1d0ad7e7fca64f35f36d92ef2d
ftgyhuji8bh <gs1wilma953@hotmail.com>
2024-05-12 18:45:33 +0700

    test

--- index.js
+++ index.js
@@ -1,30 +1,8 @@
-function numIslands(grid) {
-  if (grid.length === 0 || grid[0].length === 0) return 0;
-  const rows = grid.length;
-  const cols = grid[0].length;
-  let islands = 0;
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      if (grid[i][j] === "1") {
-        islands++;
-        dfs(i, j);
-      }
-    }
-  }
-  return islands;
-  function dfs(row, col) {
-    if (
-      row < 0 ||
-      row >= rows ||
-      col < 0 ||
-      col >= cols ||
-      grid[row][col] !== "1"
-    )
-      return;
-    grid[row][col] = "0";
-    dfs(row + 1, col);
-    dfs(row - 1, col);
-    dfs(row, col + 1);
-    dfs(row, col - 1);
-  }
+function minDepth(root) {
+  if (!root) return 0;
+  if (!root.left && !root.right) return 1;
+  let min = Infinity;
+  if (root.left) min = Math.min(min, minDepth(root.left));
+  if (root.right) min = Math.min(min, minDepth(root.right));
+  return min + 1;
 }
