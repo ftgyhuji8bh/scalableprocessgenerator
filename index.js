a3263af903da9968462b9994d36164821ab97e8f
ftgyhuji8bh <gs1wilma953@hotmail.com>
2024-05-15 07:31:16 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function quickSort(arr) {
-  if (arr.length <= 1) return arr;
-  const pivot = arr[Math.floor(arr.length / 2)];
-  const left = [];
-  const right = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < pivot) left.push(arr[i]);
-    else if (arr[i] > pivot) right.push(arr[i]);
+function solveSudoku(board) {
+  solve(board);
+  function solve(board) {
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (board[i][j] === ".") {
+          for (let num = 1; num <= 9; num++) {
+            const numChar = num.toString();
+            if (isValid(board, i, j, numChar)) {
+              board[i][j] = numChar;
+              if (solve(board)) return true;
+              board[i][j] = ".";
+            }
+          }
+          return false;
+        }
+      }
+    }
+    return true;
   }
-  return quickSort(left).concat([pivot]).concat(quickSort(right));
+  function isValid(board, row, col, num) {
+    const boxRow = Math.floor(row / 3) * 3;
+    const boxCol = Math.floor(col / 3) * 3;
+    for (let i = 0; i < 9; i++) {
+      if (
+        board[row][i] === num ||
+        board[i][col] === num ||
+        board[boxRow + Math.floor(i / 3)][boxCol + (i % 3)] === num
+      )
+        return false;
+    }
+    return true;
+  }
 }
